df5cea594114fa7e1c94a2d927f0dfd855ab459e
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-04-03 19:11:35 +0530

    Create a connection and set a driver

--- back-ends/spring-back-end/src/main/java/lk/ijse/dep13/springbackend/WebRootConfig.java
+++ back-ends/spring-back-end/src/main/java/lk/ijse/dep13/springbackend/WebRootConfig.java
@@ -1,7 +1,23 @@
 package lk.ijse.dep13.springbackend;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Configuration
 public class WebRootConfig {
+
+    public WebRootConfig() throws ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+    }
+
+    @Scope("prototype")  // create this again and again
+    @Bean
+    public Connection connection() throws SQLException {
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/dep13_note_app", "postgres", "psql");
+    }
 }
